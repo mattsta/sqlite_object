1b00e4b81b212cdb01d497b458abdcc7602b6eff
Matt Stancliff <matt@genges.com>
2020-12-04 12:28:02 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="sqlite_object",
-    version="0.3",
+    version="0.3.3",
     author_email="luke@hospadaruk.org",
     description="sqlite-backed collection objects",
     author="Luke Hospadaruk",
